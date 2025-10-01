f7abf4295da2ce123e3e9aed80ae17d3587d1507
Caio R. Saboia <103676601+caiosaboia@users.noreply.github.com>
2025-10-01 09:38:06 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,10 +10,28 @@ Import-Module Get-ChildItemColor
 Import-Module PSReadLine
 Import-Module Terminal-Icons
 
+#AUTOCOMPLETE
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle InlineView   
+Set-PSReadLineOption -Colors @{
+  Command     = 'Blue'
+  # Parameter = 'Green'
+  # Operator  = 'Yellow'
+  Variable  = 'Magenta'
+  # String    = 'DarkYellow'
+  # Number    = 'White'
+  # Type      = 'DarkCyan'
+  # Comment   = 'DarkGreen'
+  # Keyword   = 'Blue'
+  Error       = 'Red'
+  Default     = 'Yellow'
+}
 
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path($ChocolateyProfile))
+{
   Import-Module "$ChocolateyProfile"
 }
 
@@ -21,6 +39,16 @@ if (Test-Path($ChocolateyProfile)) {
 $env:GEMINI_API_KEY = ""
 
 
-function ll() { Get-ChildItem | Format-Table}
-function la() { Get-ChildItem | Format-Wide}
-function lb() { Get-ChildItem | Format-List}
+function ll()
+{ Get-ChildItem | Format-Table
+}
+function la()
+{ Get-ChildItem | Format-Wide
+}
+function lb()
+{ Get-ChildItem | Format-List
+}
+
+Set-Alias n nvim
+Set-Alias vim nvim
+
